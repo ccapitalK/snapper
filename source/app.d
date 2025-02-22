3a2d403790df7697688adbbb3faf0e6132cf7613
Sahan Fernando <sahan.h.fernando@gmail.com>
2025-02-22 17:35:33 +1100

    Basic UCI interface with logging

--- source/app.d
+++ source/app.d
@@ -1,5 +1,59 @@
+import std.algorithm;
+import std.exception;
 import std.stdio;
 
+class ChessEngine {
+    File outFile;
+    bool pipeClosed = false;
+    string lastCommand;
+
+    this() {
+         outFile = File("run.log", "w");
+    }
+
+    string readCommand() {
+        string line;
+        line = readln();
+        if (line == "") {
+            pipeClosed = true;
+            outFile.write("Closed pipe");
+        } else {
+            outFile.write("Read: ", line);
+        }
+        lastCommand = line[0 .. $ - 1];
+        return lastCommand;
+    }
+
+    string expectCommand() {
+        try {
+            return readCommand();
+        } finally {
+            enforce(!pipeClosed, "Unexpected EOF");
+        }
+    }
+
+    void sendCommand(string line) {
+        enforce(line.length > 0 && line[$ - 1] == '\n');
+        outFile.write("Wrote: ", line);
+        write(line);
+        stdout.flush();
+    }
+
+    void run() {
+        performHandshake();
+    }
+
+    void performHandshake() {
+        enforce(expectCommand() == "uci", "Driver is not a UCI chess frontend");
+        sendCommand("id name Fax\n");
+        sendCommand("id author ccapitalK\n");
+        sendCommand("uciok\n");
+        enforce(expectCommand() == "isready", "Unknown command in handshake");
+        sendCommand("readyok\n");
+    }
+}
+
 void main() {
-    writeln("Edit source/app.d to start your project.");
+    auto engine = new ChessEngine();
+    engine.run();
 }
