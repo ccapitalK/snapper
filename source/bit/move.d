28fba1ce79559e37c5fd478272698425e29e2f5a
Sahan Fernando <sahan.h.fernando@gmail.com>
2025-03-15 20:18:10 +1100

    Implement knight moves in bitboard

--- source/bit/move.d
+++ source/bit/move.d
@@ -2,6 +2,7 @@ module snapper.bit.move;
 
 import core.bitop;
 import std.logger;
+import std.range;
 
 import snapper.bit;
 import snapper.repr;
@@ -17,18 +18,70 @@ MCoord coordFromBit(ulong v) {
     return MCoord(pos % 8, pos / 8);
 }
 
+ulong bitsWhere(alias predicate)() {
+    ulong set;
+    foreach(y; 0 .. 8) {
+        foreach(x; 0 .. 8) {
+            ulong v = 1UL << (x + 8 * y);
+            if (predicate(x, y)) {
+                set |= v;
+            }
+        }
+    }
+    return set;
+}
+
+struct StaticTables {
+    // Mask for each diagonal, each diagonal spanning from top left -> bottom right
+    ulong[15] diagBottomRight;
+    // Mask for each diagonal, each diagonal spanning from top right -> bottom left
+    ulong[15] diagTopRight;
+    // Masks of bits that are exclusively in a specific direction (ignoring other axis) of index
+    ulong[8] left;
+    ulong[8] right;
+    ulong[8] up;
+    ulong[8] down;
+    // Masks of positions that are the same row/column as the index
+    ulong[8] row;
+    ulong[8] column;
+}
+
+// Initialize the static tables
+static const StaticTables STATIC_TABLES = {
+    StaticTables tables;
+    foreach (i; 0 .. 64) {
+        auto v = 1UL << i;
+        auto x = i % 8;
+        auto y = i / 8;
+        tables.diagBottomRight[x + y] |= v;
+        tables.diagTopRight[7 + x - y] |= v;
+    }
+    foreach (i; 0 .. 8) {
+        tables.left[i] = bitsWhere!((x, y) => x < i);
+        tables.right[i] = bitsWhere!((x, y) => x > i);
+        tables.up[i] = bitsWhere!((x, y) => y > i);
+        tables.down[i] = bitsWhere!((x, y) => y < i);
+        tables.row[i] = bitsWhere!((x, y) => y == i);
+        tables.column[i] = bitsWhere!((x, y) => x == i);
+    }
+    return tables;
+}();
+
 // Get mask with entire rows filled if any item in row present
 PositionMask fillRows(PositionMask input) {
     ulong set;
-    input.iterateBits!(v => set |= 0xFFUL << (8 * (v.bitPos / 8)));
+    // input.iterateBits!(v => set |= 0xFFUL << (8 * (v.bitPos / 8)));
+    input.iterateBits!(v => set |= STATIC_TABLES.row[v.bitPos / 8]);
     return PositionMask(set);
 }
 
 // Get mask with entire columns filled if any item in column present
 PositionMask fillCols(PositionMask input) {
-    const ulong pattern = 0x0101_0101_0101_0101UL;
+    // const ulong pattern = 0x0101_0101_0101_0101UL;
+    // ulong set;
+    // input.iterateBits!(v => set |= pattern << (v.bitPos % 8));
     ulong set;
-    input.iterateBits!(v => set |= pattern << (v.bitPos % 8));
+    input.iterateBits!(v => set |= STATIC_TABLES.column[v.bitPos % 8]);
     return PositionMask(set);
 }
 
@@ -41,33 +94,14 @@ unittest {
     assert(mask.fillCols.value == 0x3B3B_3B3B_3B3B_3B3BUL);
 }
 
-struct DiagonalTable {
-    // Mask for each diagonal, each diagonal spanning from top left -> bottom right
-    ulong[15] bottomRight;
-    // Mask for each diagonal, each diagonal spanning from top right -> bottom left
-    ulong[15] topRight;
-}
-
-static const DiagonalTable DIAGONAL_TABLE = {
-    DiagonalTable table;
-    foreach (i; 0 .. 64) {
-        auto v = 1UL << i;
-        auto x = i % 8;
-        auto y = i / 8;
-        table.bottomRight[x + y] |= v;
-        table.topRight[7 + x - y] |= v;
-    }
-    return table;
-}();
-
 PositionMask fillDiagonal(PositionMask input) {
     ulong set;
     input.iterateBits!((ulong v) {
         auto pos = v.bitPos;
         auto x = pos % 8;
         auto y = pos / 8;
-        set |= DIAGONAL_TABLE.bottomRight[x + y];
-        set |= DIAGONAL_TABLE.topRight[x + 7 - y];
+        set |= STATIC_TABLES.diagBottomRight[x + y];
+        set |= STATIC_TABLES.diagTopRight[x + 7 - y];
     });
     return PositionMask(set);
 }
@@ -84,20 +118,10 @@ PositionMask getDiagonalFirstIntersect(PositionMask source, PositionMask obstacl
         auto pos = v.bitPos;
         auto x = pos % 8;
         auto y = pos / 8;
-        set |= DIAGONAL_TABLE.bottomRight[x + y];
-        set |= DIAGONAL_TABLE.topRight[x + 7 - y];
+        throw new Exception("Unimplemented");
     });
     return PositionMask(set);
 }
-
-// .X.....X
-// ..X...X.
-// ...X.X..
-// ....X...
-// ...X.X..
-// ..X...X.
-// .X.....X
-// X.......
 
 unittest {
     assert(PositionMask(1).fillDiagonal.numOccupied == 8);
@@ -121,4 +145,52 @@ unittest {
     interceptMask.setPos(MCoord(6, 1), true);
     interceptMask.setPos(MCoord(3, 3), true);
     assert(bishopMask.fillDiagonal.intersection(interceptMask).numOccupied == 3);
+}
+
+PositionMask getKnightMoves(PositionMask input) {
+    ulong set;
+    static const string[2] xDirs = ["left", "right"];
+    static const string[2] yDirs = ["down", "up"];
+    // For each of the 8 knight moves
+    static foreach (sx; 0 .. 2) {
+        static foreach (sy; 0 .. 2) {
+            static foreach (dxIsLarger; 0 .. 2) {
+                {
+                    static const int udx = (dxIsLarger ? 2 : 1);
+                    static const int udy = (dxIsLarger ? 1 : 2);
+                    // Construct the knight move as a (dx, dy) tuple from the source square
+                    const int dx = udx * SIGNS[sx];
+                    const int dy = udy * SIGNS[sy];
+                    // We want to ignore source positions that would go off the board when performing this
+                    // move. Mask only the set bits in input that can perform this move.
+                    static const auto xMask = mixin("STATIC_TABLES." ~ xDirs[!sx])[sx ? 8 - udx : udx - 1];
+                    static const auto yMask = mixin("STATIC_TABLES." ~ yDirs[!sy])[sy ? 8 - udy : udy - 1];
+                    // The final calculated shift left (can be negative) that this move is equivalent to
+                    static const auto shift = dx + 8 * dy;
+                    auto validSourcePositions = input.value & xMask & yMask;
+                    static if (shift > 0) {
+                        set |= validSourcePositions << shift;
+                    } else {
+                        set |= validSourcePositions >> -shift;
+                    }
+                }
+            }
+        }
+    }
+    return PositionMask(set);
+}
+
+unittest {
+    assert(PositionMask.empty.getKnightMoves.numOccupied == 0);
+    assert(PositionMask(MCoord(0, 0)).getKnightMoves.numOccupied == 2);
+    assert(PositionMask(MCoord(7, 0)).getKnightMoves.numOccupied == 2);
+    assert(PositionMask(MCoord(7, 7)).getKnightMoves.numOccupied == 2);
+    PositionMask(MCoord(1, 1)).getKnightMoves;
+    PositionMask(MCoord(2, 2)).getKnightMoves;
+    assert(PositionMask(MCoord(1, 1)).getKnightMoves.numOccupied == 4);
+    assert(PositionMask(MCoord(1, 2)).getKnightMoves.numOccupied == 6);
+    assert(PositionMask(MCoord(2, 2)).getKnightMoves.numOccupied == 8);
+    assert(PositionMask(MCoord(5, 5)).getKnightMoves.numOccupied == 8);
+    assert(PositionMask(MCoord(6, 5)).getKnightMoves.numOccupied == 6);
+    assert(PositionMask(MCoord(2, 2)).getKnightMoves.value == 0x0000_000A_1100_110AUL);
 }
